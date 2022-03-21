5ee774a78e6ea98f71784613f18fcb0be5b24a08
K-taiga <taigakatou0329@gmail.com>
2022-03-21 16:30:37 +0900

    [add] ECS用のcloudwatchのlogを設定

---
+++ envs/prod/log/laravel-fargate-app/cloudwatch_log.tf
@@ -0,0 +1,10 @@
+resource "aws_cloudwatch_log_group" "nginx" {
+  name = "/ecs/${local.name_prefix}-${local.service_name}/nginx"
+  # ログの保存期間
+  retention_in_days = 90
+}
+
+resource "aws_cloudwatch_log_group" "php" {
+  name              = "/ecs/${local.name_prefix}-${local.service_name}/php"
+  retention_in_days = 90
+}
