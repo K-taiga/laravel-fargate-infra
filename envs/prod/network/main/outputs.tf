2599c3ecd3ff74d8a7923e82339fe00442cf2e8c
K-taiga <taigakatou0329@gmail.com>
2022-04-03 19:18:03 +0900

    [add] RDS用のprivate subnetを追加

--- envs/prod/network/main/outputs.tf
+++ envs/prod/network/main/outputs.tf
@@ -4,6 +4,10 @@ output "security_group_web_id" {
 output "security_group_vpc_id" {
   value = aws_security_group.vpc.id
 }
+
+output "security_group_db_laravel-fargate-app_id" {
+  value = aws_security_group.db_laravel-fargate-app.id
+}
 output "subnet_public" {
   value = aws_subnet.public
 }
@@ -14,4 +18,8 @@ output "subnet_private" {
 
 output "vpc_this_id" {
   value = aws_vpc.this.id
+}
+
+output "db_subnet_group_this_id" {
+  value = aws_db_subnet_group.this.id
 }
