1c8e2788fc8e31340df15617c74ed61af8159b0c
K-taiga <taigakatou0329@gmail.com>
2022-04-30 16:53:25 +0900

    [add] ElastiCacheのCNAMEレコードを追加

---
+++ envs/prod/routing/cache_internal/data.tf
@@ -0,0 +1,18 @@
+data "terraform_remote_state" "network_main" {
+  backend = "s3"
+  config = {
+    bucket = "k-taiga-tfstate"
+    key    = "${local.system_name}/${local.env_name}/network/main_v1.0.0.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+data "terraform_remote_state" "cache_laravel-fargate-app" {
+  backend = "s3"
+
+  config = {
+    bucket = "k-taiga-tfstate"
+    key    = "${local.system_name}/${local.env_name}/cache/laravel-fargate-app_v1.0.0.tfstate"
+    region = "ap-northeast-1"
+  }
+}
---
+++ envs/prod/routing/cache_internal/route53.tf
@@ -0,0 +1,17 @@
+resource "aws_route53_zone" "this" {
+  name = "cache.internal"
+  vpc {
+    vpc_id = data.terraform_remote_state.network_main.outputs.vpc_this_id
+  }
+}
+
+resource "aws_route53_record" "cache_name" {
+  zone_id = aws_route53_zone.this.zone_id
+  name = "cache.${aws_route53_zone.this.name}"
+  type = "CNAME"
+  ttl = 300
+
+  records = [
+      data.terraform_remote_state.cache_laravel-fargate-app.outputs.elasticache_replication_group_this_primary_endpoint_address
+  ]
+}
